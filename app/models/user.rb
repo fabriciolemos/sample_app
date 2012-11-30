9aa3a6cbc5bf4d2dc97f0f479b97896c5a77867c
Fabricio Lemos <fabricio.lemos@gmail.com>
2012-11-30 09:37:57 -0300

    Testcase for email address with mixed case

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 end
